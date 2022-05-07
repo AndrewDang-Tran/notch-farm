f6c71cfef2370fd72160132f5dbec6247522235d
Andrew Dang-Tran <andrewdt1506@gmail.com>
2022-05-07 16:24:57 -0700

    feat(): support healthcheck query

--- notch-server/src/main.rs
+++ notch-server/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct HealthResponse<'a> {
+    status: &'a str
+}
+const HEALTHY_STATUS: &str = "pass";
+const HEALTH_RESPONSE: HealthResponse = HealthResponse {
+    status: HEALTHY_STATUS
+};
+
+#[get("/health-check")]
+async fn health_check() -> impl Responder {
+    web::Json(HEALTH_RESPONSE)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
