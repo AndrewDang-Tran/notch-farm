0d0d3055d24341b8c231f53d19a57c1510954c70
Andrew Dang-Tran <andrewdt1506@gmail.com>
2022-05-07 17:53:51 -0700

    rename() controllers to routes

--- notch-server/src/main.rs
+++ notch-server/src/main.rs
@@ -1,13 +1,13 @@
 use actix_web::{App, HttpServer, middleware};
 
-mod controllers;
+mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
-            .configure(controllers::init_health_controller)
+            .configure(routes::init_health_controller)
     })
         .bind(("127.0.0.1", 8080))?
         .run()
